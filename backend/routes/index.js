8184c9f126bd5dab835b9b90acfb6f018ca64c73
Benjamin Lau <benjaminkhlau@gmail.com>
2022-07-27 17:52:17 -0500

    phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,4 +19,14 @@ router.get("/api/csrf/restore", (req, res) => {
   });
   // ...
   
+  // backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
+
+// backend/routes/api/index.js
+// ...
 module.exports = router;
