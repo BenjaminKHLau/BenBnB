940fdd8228b43a5c6e127f4c7033aa40db76bdf0
Benjamin Lau <benjaminkhlau@gmail.com>
2022-07-29 17:31:59 -0500

    add Heroku dependencies

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -57,10 +57,7 @@ const validateSignup = [
 // ...
 
 // Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ email, username, password });
   
