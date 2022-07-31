e9e7c16611c2dbdbf9790c6c39e5f24fada16b7a
Benjamin Lau <benjaminkhlau@gmail.com>
2022-07-31 18:41:43 -0500

    seeds finished

--- backend/db/seeders/20220731231447-images.js
+++ backend/db/seeders/20220731231447-images.js
@@ -1,7 +1,45 @@
-'use strict';
+"use strict";
+
+const images = [
+  {
+    userId: 1,
+    spotId: 1,
+    reviewId: 1,
+    previewImage: 1,
+    url: "centralpark1",
+  },
+  {
+    userId: 2,
+    spotId: 2,
+    reviewId: 2,
+    previewImage: 1,
+    url: "centralpark2",
+  },
+  {
+    userId: 3,
+    spotId: 3,
+    reviewId: 3,
+    previewImage: 1,
+    url: "centralpark3",
+  },
+  {
+    userId: 4,
+    spotId: 4,
+    reviewId: 4,
+    previewImage: 1,
+    url: "centralpark4",
+  },
+  {
+    userId: 5,
+    spotId: 5,
+    reviewId: 5,
+    previewImage: 1,
+    url: "centralpark5",
+  },
+];
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,15 +48,17 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    await queryInterface.bulkInsert("Images", images);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    await queryInterface.bulkDelete("Images", images);
+  },
 };
