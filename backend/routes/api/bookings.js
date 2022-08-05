6c0ff6d3aa0aba88edc99ce8fe0c7f79e241bd86
Benjamin Lau <benjaminkhlau@gmail.com>
2022-08-05 12:10:02 -0500

    fixed bug in delete existing booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -123,7 +123,7 @@ router.delete('/:bookingId', requireAuth, async(req, res, next) => {
     if(bookingDie.userId !== req.user.id){
         const err = new Error("This is not your booking")
         err.status = 403
-        err.errors = [`Spot with ID ${req.params.spotId} does not exist`]
+        err.errors = [`Booking with ID ${req.params.bookingId} does not exist`]
         return next(err)
     }
 
