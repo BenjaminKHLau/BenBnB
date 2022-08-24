23aff36d14ebb87fa6032f7b8d77ab6a89464831
Benjamin Lau <benjaminkhlau@gmail.com>
2022-08-24 16:39:38 -0500

    did a little bit of css styling God help me plz

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect, NavLink } from "react-router-dom";
 import { getAllSpotsThunk } from "../../store/spots";
+import "./AllSpots.css"
 
 
 function GetAllSpots(){
@@ -17,22 +18,32 @@ function GetAllSpots(){
     // if(!allSpots){return null}
 
     return (
-        <div>
-            <ul>
+        <div className="main-container">
+
+            <div className="middle-container">
                 {normalSpots.map(spot => (
-                <div className="splash-spot-container">
-
-                    <img src={spot?.previewImage || "https://hgtvhome.sndimg.com/content/dam/images/hgtv/unsized/2017/9/29/CI_TTMK_Charing-Cross-Road-1.jpg"} className="splash-spot-images"></img>
-                    <li key={spot?.id}>
+                    <div className="inner-container">
                    <NavLink to={`/spots/${spot.id}`}>
-                        {spot?.name}
-                   </NavLink>
-                        </li>
 
-                    <p>Price: ${spot?.price}/night</p>
-                </div>
+                    <div className="just-the-image">
+                    <img src={spot?.previewImage || "https://hgtvhome.sndimg.com/content/dam/images/hgtv/unsized/2017/9/29/CI_TTMK_Charing-Cross-Road-1.jpg"} className="splash-spot-images"></img>
+                    </div>
+
+                    {/* <div key={spot?.id}> */}
+                    <div className="individual-spot-info">
+                        {spot?.name} {"| "} 
+                        {spot?.city} {"| "}
+                        {spot?.state}
+                        </div>
+                   <p>${spot?.price} night</p>
+                   </NavLink>
+                   {/* <div className="just-the-price"> */}
+                   {/* </div> */}
+
+                    </div>
                 ))}
-            </ul>
+                </div>
+
         </div>
     )
 }
