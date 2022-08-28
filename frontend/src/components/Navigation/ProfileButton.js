dc9f987a7865c4847fbb8984fd103ebff5cfafd3
Benjamin Lau <benjaminkhlau@gmail.com>
2022-08-27 19:42:49 -0500

    css done for dropdown menu profile

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,11 +1,12 @@
 // frontend/src/components/Navigation/ProfileButton.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import CreateSpotModal from "../CreateNewSpot/NewSpotModal";
 import "./Navigation.css"; //added second
 import menupic from "./menu-pic.png"
+
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -39,32 +40,38 @@ function ProfileButton({ user }) {
 
   return (
     <div className="ProfileButtonjs">
-      {/* <div className="create-spot-modal"> */}
-        <CreateSpotModal />
-      {/* </div> */}
 
-      {/* <div className="NavBar2"> */}
+        <CreateSpotModal />
+
+
+
         <div onClick={openMenu} className="profile-button-nav">
-        <img src={menupic} className="menupic">
-          {/* <div className="fas fa-user-circle" /> */}
-        </img>
+        <img src={menupic} className="menupic"/>
         </div>
 
         {showMenu && (
           <div className="profile-dropdown">
-            <div className="your-spots-link">
+            {/* <div className="your-spots-link">
             <Link to="/spots/current">Your Spots</Link>
+            </div> */}
+            <Link to="/spots/current">
+            <div className="your-spots-link">
+              Your Spots
             </div>
-            <div className="your-reviews-link">
-            <Link to="/reviews/current">Your Reviews</Link>
-            </div>
+              </Link>
 
-            <div>{user.username}</div>
-            <div>{user.email}</div>
-            <div><button onClick={logout}>Log Out</button></div>
+            <Link to="/reviews/current">
+            <div className="your-reviews-link">
+              Your Reviews
+            </div>
+            </Link>
+
+            <div className="your-username">{user.username}</div>
+            <div className="your-email">{user.email}</div>
+            {/* <div className="logout-button"><div onClick={logout}>Log Out</div></div> */}
+            <div className="logout-button" onClick={logout}>Log Out</div>
           </div>
         )}
-      {/* </div> */}
     </div>
   );
 }
