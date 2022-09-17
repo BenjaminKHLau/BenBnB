09c63c46a912726b66a9be54c36f5fb85dbdecba
Benjamin Lau <benjaminkhlau@gmail.com>
2022-09-16 23:08:59 -0500

    footer stuff

--- frontend/src/components/Navigation/Footer.js
+++ frontend/src/components/Navigation/Footer.js
@@ -1,6 +1,7 @@
 import mail from "./mail.png"
 import linkedin from "./linkedin.png"
 import kiwi from "./kiwi.jpg"
+import github from "./github.png"
 
 function Footer() {
 
@@ -9,12 +10,15 @@ function Footer() {
             <h2>Contact Me!</h2>
             <div className="footer-stuff">
 
-            <a href="https://www.linkedin.com/in/benjaminkhlau/" className="hire-me">
+            <a href="https://www.linkedin.com/in/benjaminkhlau/" target="_blank" rel="noopener noreferrer" className="hire-me">
                 <img src={linkedin} className="footer-img"/>
                 {/* Hire Me! Click for my LinkedIn! */}
             </a>
             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
                 <img src={kiwi} className="footer-img" />
+            </a>
+            <a href="https://github.com/BenjaminKHLau" target="_blank" rel="noopener noreferrer">
+                <img src={github} className="footer-img" />
             </a>
             <a href="mailto:benjaminkhlau@gmail.com" className="hire-email">
                 <img src={mail} className="footer-img" />
